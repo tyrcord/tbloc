3a66ea051bf2f564c53a847c5f8480fa5dd158a3
Thibault Zanini <thibaultzanini@gmail.com>
2019-05-13 18:04:08 -0400

    Add missing exports

--- packages/core/src/types/index.ts
+++ packages/core/src/types/index.ts
@@ -1,8 +1,6 @@
-export * from './bidirectional-bloc-transition.type';
 export * from './bidirectional-bloc-delegate.type';
-
-export * from './unidirectional-bloc-transition.type';
-export * from './unidirectional-bloc-delegate.type';
-
+export * from './bidirectional-bloc-transition.type';
 export * from './bloc-event.type';
 export * from './omit.type';
+export * from './unidirectional-bloc-delegate.type';
+export * from './unidirectional-bloc-transition.type';
--- packages/forms/src/core/index.ts
+++ packages/forms/src/core/index.ts
@@ -1,3 +1,4 @@
+export * from './form-bloc-state.builder';
 export * from './form-bloc.delegate';
 export * from './form-bloc.state';
 export * from './form.bloc';
